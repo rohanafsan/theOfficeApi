109768135c6a723fe6c60b287e99296283b1ffe2
notmrafsan <mrafsan@edu.uwaterloo.ca>
2020-03-13 11:22:24 -0400

    procfile

--- db/index.js
+++ db/index.js
@@ -1,24 +1,24 @@
 const { Pool } = require ('pg');
-// const { user, host, database, password, port } = require('../secrets/db_configuration')
+const { user, host, database, password, port } = require('../secrets/db_configuration')
 
-// const pool = new Pool({user, host, database, password, port});
+const pool = new Pool({user, host, database, password, port});
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-});
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: true
+// });
 
-router.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-});
+// router.get('/db', async (req, res) => {
+//     try {
+//       const client = await pool.connect()
+//       const result = await client.query('SELECT * FROM test_table');
+//       const results = { 'results': (result) ? result.rows : null};
+//       res.render('pages/db', results );
+//       client.release();
+//     } catch (err) {
+//       console.error(err);
+//       res.send("Error " + err);
+//     }
+// });
 
 module.exports = pool;
