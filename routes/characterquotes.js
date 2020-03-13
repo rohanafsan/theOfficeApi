25e523d9c2ff0c547038379a4eef4aa57116704a
notmrafsan <mrafsan@edu.uwaterloo.ca>
2020-03-13 13:36:56 -0400

    procfile

--- routes/characterquotes.js
+++ routes/characterquotes.js
@@ -3,18 +3,18 @@ const { Router } = require('express');
 const pool = require('../db');
 const router = Router();
 
-router.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-});
+// router.get('/db', async (req, res) => {
+//     try {
+//       const client = await pool.connect()
+//       const result = await client.query('SELECT * FROM test_table');
+//       const results = { 'results': (result) ? result.rows : null};
+//       res.render('pages/db', results );
+//       client.release();
+//     } catch (err) {
+//       console.error(err);
+//       res.send("Error " + err);
+//     }
+// });
 
 router.get('/', (request, response, next) => {
     pool.query('SELECT * FROM characterquotes', (err, res) => {
